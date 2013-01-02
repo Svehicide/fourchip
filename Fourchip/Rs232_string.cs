7f348b00761172c8f5c129abb0045ecabd65b5df
ludovic.calmant@gmail.com <ludovic.calmant@gmail.com@ebad10a5-b3ee-7e8b-11e4-015f4365d617>
2013-01-02 14:52:03 +0000

    RS232 communication codes

---
+++ Fourchip/Rs232_string.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fourchip
+{
+    class Rs232_string
+    {
+        public const String HELLO = "#00@";                     //#00@
+        public const String SCAN = "#01@";                      //#01@Firstname;Name
+        public const String LOGIN = "#02@";                     //#02@TRUE ou #02@FALSE
+        
+        public const String CARD_IP = "#31@";                   //#31@x.x.x.x
+        public const String CARD_NETWORK = "#32@";              //#32@x.x.x.x
+        public const String CARD_GW = "#33@";                   //#33@x.x.x.x
+        public const String CARD_DNS1 = "#34@";                 //#34@x.x.x.x
+        public const String CARD_DNS2 = "#35@";                 //#35@x.x.x.x
+
+        public const String TEMP = "#04@";                      //#04@tempValue
+        
+        public const String LIGHT = "#05@";                     //#05@lightValue
+        
+        public const String LED = "#06@";                       //#06@ledFreq
+    }
+}
