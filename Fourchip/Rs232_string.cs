658abc7cc0fa0e592a2f32b326e688a5196e540f
ludovic.calmant@gmail.com <ludovic.calmant@gmail.com@ebad10a5-b3ee-7e8b-11e4-015f4365d617>
2013-01-02 15:21:26 +0000

    LOGIN code syntax changed

--- Fourchip/Rs232_string.cs
+++ Fourchip/Rs232_string.cs
@@ -9,7 +9,7 @@ namespace Fourchip
     {
         public const String HELLO = "#00@";                     //#00@
         public const String SCAN = "#01@";                      //#01@Firstname;Name
-        public const String LOGIN = "#02@";                     //#02@TRUE ou #02@FALSE
+        public const String LOGIN = "#02@";                     //#02@1 ( OK ) ou #02@0 ( NOT OK ) ou #02@2 ( TIME OUT )
         
         public const String CARD_IP = "#31@";                   //#31@x.x.x.x
         public const String CARD_NETWORK = "#32@";              //#32@x.x.x.x
